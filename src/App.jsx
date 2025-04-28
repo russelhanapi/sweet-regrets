be5a2e1955e31efb60b511aef7d2fa839c336857
russelhanapi <rphanapi@gmail.com>
2025-04-28 10:33:30 +0800

    feat: add pages and configure routing​
    - Created Home, Menu, Cart, and Order pages.
    - Configured routing using createBrowserRouter and RouterProvider.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Cart from './pages/Cart';
+import Order from './pages/Order';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/menu', element: <Menu /> },
+  { path: '/cart', element: <Cart /> },
+  { path: '/order', element: <Order /> },
+]);
+
 function App() {
-  return <h1 className='text-success text-3xl'>Init</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
