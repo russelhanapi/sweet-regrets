7ad9bc03db17b81e9b7b2b32de5e90c02a719660
russelhanapi <rphanapi@gmail.com>
2025-04-28 09:55:10 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className='text-success text-3xl'>Init</h1>;
+}
+
+export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+import brandLogoMobile from "./sweet-regrets-logo-mobile.svg";
+import brandLogoWeb from "./sweet-regrets-logo-web.svg";
+
+export { brandLogoMobile, brandLogoWeb };
