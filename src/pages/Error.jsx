40722137033ab3d0ae40e195c249b07ac6a7ef53
russelhanapi <rphanapi@gmail.com>
2025-04-30 23:00:24 +0800

    feat: add error message display in Error component

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,14 +3,14 @@ import LinkButton from '../components/ui/LinkButton';
 
 function Error() {
   const error = useRouteError();
-  console.log(error);
+
   return (
     <div className='bg-secondary text-secondary-content min-h-screen px-4 py-8 sm:py-8 md:px-8'>
       <h1 className='mb-4 text-2xl font-medium sm:text-4xl'>
         Oops! That didn’t work 😅
       </h1>
       <p className='mb-4'>
-        {error.status} {error.data}
+        {error.status} {error.data || error.message}
       </p>
       <LinkButton to='-1'>Go Back</LinkButton>
     </div>
